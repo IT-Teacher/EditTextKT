10f4515a8e886c24c1a6f878a59c5f330e1fae54
Admin <mirgiyozom@gmail.com>
2022-04-05 16:15:15 +0500

    setOnkeyListener

---
+++ app/src/main/java/com/example/edittextkt/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.edittextkt
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.name)
+        val et2 = findViewById<EditText>(R.id.password)
+
+        val button = findViewById<Button>(R.id.button)
+
+        val textView = findViewById<TextView>(R.id.tv1)
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                textView.text = s
+            }
+        })
+
+        editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                when (keyCode) {
+                    KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                        et2.requestFocus()
+                        return@OnKeyListener true
+                    }
+                    else -> {
+                    }
+                }
+            }
+            false
+        })
+
+
+
+
+    }
+}
